f68f7e18d4da6b3f649e12fcd96d8b052758c5df
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-10-02 23:27:54 +0200

    Verify ssl in chef-solo

--- solo.rb
+++ solo.rb
@@ -1,3 +1,4 @@
 log_level                :info
 log_location             STDOUT
 cookbook_path            File.join(File.dirname(File.expand_path(__FILE__)), 'cookbooks')
+ssl_verify_mode          :verify_peer

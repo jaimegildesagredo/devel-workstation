d0e2e40b9d847fa5b95041d8f2472af2c34cc367
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-10-03 01:40:45 +0200

    package recipe for the_silver_searcher cookbook

--- cookbooks/the_silver_searcher/recipes/package.rb
+++ cookbooks/the_silver_searcher/recipes/package.rb
@@ -1,8 +1,10 @@
 #
 # Cookbook Name:: the_silver_searcher
-# Recipe:: default
+# Recipe:: package
 #
 # Copyright 2014, Jaime Gil de Sagredo
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "the_silver_searcher"

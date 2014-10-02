243a8baaba1db64ea679bf013f53de0ea092024b
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-10-02 22:50:02 +0200

    Set default cookbook metadata

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,3 +5,6 @@ syntax_check_cache_path  '/home/jaimegildesagredo/devel-workstation/.chef/syntax
 cookbook_path            [
   File.join(File.dirname(__FILE__), '..', 'cookbooks')
 ]
+cookbook_copyright       'Jaime Gil de Sagredo'
+cookbook_email           'jaimegildesagredo@gmail.com'
+cookbook_license         'apachev2'

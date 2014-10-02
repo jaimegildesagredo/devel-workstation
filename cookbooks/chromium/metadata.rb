bbdbea6176a5a724f3a50af10957ce81aba31977
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-10-03 01:40:05 +0200

    Chromium

---
+++ cookbooks/chromium/metadata.rb
@@ -0,0 +1,8 @@
+name             'chromium'
+maintainer       'Jaime Gil de Sagredo'
+maintainer_email 'jaimegildesagredo@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures chromium'
+version          '0.1.0'
+
+depends          'portage', '=0.3.2'

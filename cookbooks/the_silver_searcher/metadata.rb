edfb389d2697ef47c30f7f2d66fc2c16ae2202b7
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-10-02 21:52:21 +0200

    the_silver_searcher cookbook

---
+++ cookbooks/the_silver_searcher/metadata.rb
@@ -0,0 +1,6 @@
+name             'the_silver_searcher'
+maintainer       'Jaime Gil de Sagredo'
+maintainer_email 'jaimegildesagredo@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures the_silver_searcher'
+version          '0.1.0'
---
+++ cookbooks/the_silver_searcher/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: the_silver_searcher
+# Recipe:: default
+#
+# Copyright 2014, Jaime Gil de Sagredo
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+package "the_silver_searcher"
